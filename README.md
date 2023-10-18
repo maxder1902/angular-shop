e7ca47c7b206a9e72fbdb1d6dc3fa9e8145b946e
maxder1902 <107987427+maxder1902@users.noreply.github.com>
2023-10-18 12:59:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-shop
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/maxder1902/angular-shop)
---
+++ src/app/cart.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  items: Product[] = [];
+  constructor(private http: HttpClient) {}
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+}
